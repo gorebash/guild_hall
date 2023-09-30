64bb9e35ce51fb781abc60beb86c8db553e36147
gorebash <brent.gore@gmail.com>
2023-09-30 14:38:40 -0700

    message save, general styles

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,21 @@
 class MessagesController < ApplicationController
-  def create; end
+  def create
+    message = current_user.messages.build(message_params)
+    if message.save
+      redirect_to chatroom_path
+    else
+      flash[:error] = 'Could not save the message at this time'
+      redirect_to chatroom_path, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def message_render(message)
     render(partial: 'message', locals: { message: })
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
