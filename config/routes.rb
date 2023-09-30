155099d35b276878b52fb1d5479348c6d9c0dab3
gorebash <brent.gore@gmail.com>
2023-09-30 07:51:40 -0700

    user login setup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'chatroom#index'
   post 'message', to: 'messages#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # post 'signup', to: 'users#create'
 end
