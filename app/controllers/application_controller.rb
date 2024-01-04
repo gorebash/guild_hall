9ecf8cc8b95a3659023f651abe7219612df5df99
Brent Gore <brent.gore@gmail.com>
2024-01-04 08:48:44 -0800

    converted existing login to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?, :current_user
+  # helper_method :logged_in?, :current_user
 
-  def logged_in?
-    session[:user_id] != nil
-  end
+  # def logged_in?
+  #   session[:user_id] != nil
+  # end
 
-  def current_user
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 
-  def require_user
-    return if logged_in?
+  # def require_user
+  #   return if logged_in?
 
-    flash[:error] = 'You must be logged in to perform that action'
-    redirect_to login_path
-  end
+  #   flash[:error] = 'You must be logged in to perform that action'
+  #   redirect_to login_path
+  # end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   validates :body, presence: true
-  #belongs_to :user
+  belongs_to :user
 
   scope :custom_display, -> { order(:created_at).last(20) }
 
