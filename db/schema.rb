8995ee511de321add6a593a988f6b28fe049edd2
Brent Gore <brent.gore@gmail.com>
2024-01-15 14:38:02 -0800

    guild entity scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_13_233340) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_223212) do
+  create_table "guilds", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
