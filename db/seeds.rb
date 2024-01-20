ce4ec9503c21b7f32b909b5d938d3ef70aca25c3
gorebash <brent.gore@gmail.com>
2024-01-20 09:00:23 -0800

    db seeds and guild index member associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+users = User.create([
+  { email: "brent.gore@gmail.com", username: "heavymetal", birth_date: "1980-01-01", password: "pass123" },
+  { email: "brent1@example.com", username: "brentclone1", birth_date: "2010-01-01", password: "pass123" },
+])
+
+guilds = Guild.create([
+  { name: "Bananaramas", description: "Its all about bananas" },
+  { name: "Coram Deo Co-op", description: "Homeschool coop" },
+  { name: "Breakfast club", description: "Detention hall mostly" },
+])
+
+GuildMember.create([
+  { user_id: users[0].id, guild_id: guilds[0].id },
+  { user_id: users[0].id, guild_id: guilds[1].id },
+  { user_id: users[1].id, guild_id: guilds[1].id },
+  { user_id: users[1].id, guild_id: guilds[2].id },
+])
