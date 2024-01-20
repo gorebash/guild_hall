56079be6e062323c0c876bd33325325a86dd5bd4
gorebash <brent.gore@gmail.com>
2024-01-20 09:31:40 -0800

    Moved chatroom to show guild view

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @message = Message.new
-    @messages = Message.custom_display
+    #@message = Message.new
+    #@messages = Message.custom_display
   end
 end
