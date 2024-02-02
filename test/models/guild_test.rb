fd7ae9da53040061bb8f1e3cba854a9b1439ffae
Brent Gore <brent.gore@gmail.com>
2024-02-02 08:02:43 -0800

    started model tests

--- test/models/guild_test.rb
+++ test/models/guild_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class GuildTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "should not create guild without description" do
+    @guild = Guild.new
+    assert_not Guild.save, "saved guild without description"
+  end
+
+  # test "guild should generate an invite code" do
+  #   @guild = Guild.new(name:"guild 1", description: "guild 1 desc")
+  #   assert @guild.invite_code
+  #   assert @guild.valid?
   # end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "message requires body" do
+    @message = Message.new
+    assert_not @message.save, "saved message with no body"
+  end
 end
