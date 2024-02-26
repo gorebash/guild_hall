58e63f0b45247e1ab20f677ea3a5f2ed2198b532
Brent Gore <brent.gore@gmail.com>
2024-02-26 08:06:56 -0800

    navbar icon alignment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,17 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "channels"
 import "bootstrap"
-
-
-// function scroll_bottom () {
-//   if ($('#messages-outer').length > 0) {
-//     $('#messages-outer').scrollTop($('#messages-outer')[0].scrollHeight);
-//   }
-// }
-
-
-// $(document).on('turbo:load', function() {
-//   $('.ui.dropdown').dropdown();
-
-//   scroll_bottom();
-// });
