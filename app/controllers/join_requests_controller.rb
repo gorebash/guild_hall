6e44d50baecafb56ebaa806fc0831ba91578ed77
gorebash <brent.gore@gmail.com>
2024-04-16 21:05:09 -0700

    style cleanup

--- app/controllers/join_requests_controller.rb
+++ app/controllers/join_requests_controller.rb
@@ -2,6 +2,7 @@ class JoinRequestsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_permission, only: %i[ update ]
   before_action :set_join_request, only: %i[ show edit update destroy ]
+  after_action :clear_session, only: %i[ create update ]
 
   # GET /join_requests or /join_requests.json
   def index
@@ -28,8 +29,8 @@ class JoinRequestsController < ApplicationController
     
     @join_request = JoinRequest.new(join_request_params)
     @join_request.user = current_user
-    @join_request.guild = find_guild_by_code @join_request.invite_code
     @join_request.invite_code = @join_request.invite_code.upcase
+    @join_request.guild = find_guild_by_code @join_request.invite_code
 
     if (@join_request.guild == nil)
       flash[:danger] = "That guild does not exist."
@@ -44,7 +45,7 @@ class JoinRequestsController < ApplicationController
     respond_to do |format|
       if @join_request.save
         flash[:success] = "Your request to join was successfully sent."
-        format.html { redirect_to join_requests_url(@join_request) }
+        format.html { redirect_to new_join_request_url }
         format.json { render :show, status: :created, location: @join_request }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -111,5 +112,9 @@ class JoinRequestsController < ApplicationController
 
   def find_guild_by_code (invite_code)
     Guild.where(invite_code: invite_code).take
+  end
+
+  def clear_session
+    session[:join_request_count] = nil
   end
 end
