c8f94e88a8d6cecbeed552d7c9712a2481e2fe45
Brent Gore <brent.gore@gmail.com>
2025-03-08 13:11:27 -0800

    backed out push subscriber tests

--- test/controllers/push_subscribers_controller_test.rb
+++ test/controllers/push_subscribers_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class PushSubscribersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get push_subscribers_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get push_subscribers_index_url
+  #   assert_response :success
+  # end
 end
